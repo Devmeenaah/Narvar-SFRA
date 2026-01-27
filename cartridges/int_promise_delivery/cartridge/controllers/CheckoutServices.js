e00db5dfdcf5ac2871305cf4726a9da0cea76c62
Aminah Yussuf <>
2026-01-27 16:54:17 -0700

    update order custom object  - fix error

--- cartridges/int_promise_delivery/cartridge/controllers/CheckoutServices.js
+++ cartridges/int_promise_delivery/cartridge/controllers/CheckoutServices.js
@@ -5,17 +5,18 @@ var server = require('server');
 server.extend(module.superModule);
 
 server.append('PlaceOrder', function (req, res, next) {
-    var viewData = res.getViewData();
+    var Transaction = require('dw/system/Transaction');
 
+    var viewData = res.getViewData();
     if (!viewData.error && viewData.orderID) {
-        var productListHelper = require('*/cartridge/scripts/productList/productListHelpers');
-        var collections = require('*/cartridge/scripts/util/collections');
         var OrderMgr = require('dw/order/OrderMgr');
 
         var order = OrderMgr.getOrder(viewData.orderID);
         //update order custom attribute 
-        order.custom.customerSelectedDeliveryDate =  session.privacy.customerSelectedDeliveryDate; 
-        
+        Transaction.wrap(function () {
+            order.custom.customerSelectedDeliveryDate =  session.privacy.customerSelectedDeliveryDate; 
+            session.privacy.customerSelectedDeliveryDate = '';
+    });
     }
 
     next();
